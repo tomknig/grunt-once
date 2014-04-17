e925041d41da0b271250efc9649eb306f4bc5538
TomKnig <hi@tomknig.de>
2014-04-17 19:45:52 +0200

    replace whitespaces with tabs

--- tasks/once.js
+++ tasks/once.js
@@ -10,49 +10,52 @@
 
 module.exports = function (grunt) {
 
-    grunt.registerMultiTask('once', 'Open HTML files with PhantomJS and save their DOM to a new file when certain criteria are met.', function () {
-        var done, phantom, binPath, which,
-            path = require('path'),
-            asset = path.join.bind(null, __dirname, '..');
-        
-        try {
-            which = require('which');
-            binPath = which.sync('phantomjs');
-        } catch (e) {
-            binPath = require('phantomjs').path;
-        }
+	grunt.registerMultiTask('once', 'Open HTML files with PhantomJS and save their DOM to a new file when certain criteria are met.', function () {
+		var done,
+			phantom,
+			binPath,
+			which,
+			path = require('path'),
+			asset = path.join.bind(null, __dirname, '..');
 
-        done = this.async();
-        
-        this.files.forEach(function (f) {
-            var src = f.src.filter(function (filepath) {
-                if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('Source file "' + filepath + '" not found.');
-                    return false;
-                } else {
-                    return true;
-                }
-            });
+		try {
+			which = require('which');
+			binPath = which.sync('phantomjs');
+		} catch (e) {
+			binPath = require('phantomjs').path;
+		}
 
-            if (src.length !== 1) {
-                grunt.log.warn('Please specify exactly one source file.');
-                return;
-            }
+		done = this.async();
 
-            src.forEach(function (file) {
-                var code = grunt.file.read(file),
-                    tempfile = f.dest + '~tmp';
-                
-                phantom = grunt.util.spawn({
-                    cmd: binPath,
-                    args: [asset('lib/phantom.js'), path.join(__dirname, '../../../', file), tempfile]
-                }, function () {
-                    var html = grunt.file.read(tempfile);
-                    grunt.file.delete(tempfile);
-                    grunt.file.write(f.dest, html);
-                    done();
-                });
-            });
-        });
-    });
+		this.files.forEach(function (f) {
+			var src = f.src.filter(function (filepath) {
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			});
+
+			if (src.length !== 1) {
+				grunt.log.warn('Please specify exactly one source file.');
+				return;
+			}
+
+			src.forEach(function (file) {
+				var code = grunt.file.read(file),
+					tempfile = f.dest + '~tmp';
+
+				phantom = grunt.util.spawn({
+					cmd: binPath,
+					args: [asset('lib/phantom.js'), path.join(__dirname, '../../../', file), tempfile]
+				}, function () {
+					var html = grunt.file.read(tempfile);
+					grunt.file.delete(tempfile);
+					grunt.file.write(f.dest, html);
+					done();
+				});
+			});
+		});
+	});
 };
